e05840e21e29d2335ec7ee03bac4efdf130f7bc8
Jason Rylance <ecnalyr@gmail.com>
2013-02-05 06:08:48 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngelWood::Application.config.secret_token = '81d28acddcef6ace79794d1f72e763d7cfda708d752a0bbc3b3578c5579c18119ad16e9ad32647950f987f8757bb1b87db5162b5ef60f83be649506cd2476af7'
