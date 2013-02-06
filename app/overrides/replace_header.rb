801da2aeac2510342db8e942bd8630b14f666306
Jason Rylance <ecnalyr@gmail.com>
2013-02-05 19:19:20 -0600

    More misc changes, need to replace "out of stock" with "sold" next.

--- app/overrides/replace_header.rb
+++ app/overrides/replace_header.rb
@@ -5,7 +5,7 @@ Deface::Override.new( :virtual_path => "spree/shared/_header",
                                         <div class="navigation">
                                           <div class="custom_logo block"><%= link_to AngelWood, root_path %></div>
                                           <div class="block blank_space"></div>
-                                          <div class="block"><%= link_to "Gallery", root_path %></div>
+                                          <div class="block no_bottom"><%= link_to "Gallery", root_path %></div>
                                           <div class="block blank_space">
                                             <li id="link-to-cart" data-hook><%= link_to_cart %></li>
                                           </div>
--- app/overrides/replace_products_ul.rb
+++ app/overrides/replace_products_ul.rb
@@ -4,14 +4,11 @@ Deface::Override.new( :virtual_path => "spree/shared/_products",
                         :text => %q{<ul id="products" class="inline product-listing" data-hook>
                                       <% products.each do |product| %>
                                         <% if product.on_display? %>
-                                          <div id="product_<%= product.id %>" class="block" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
+                                          <div id="product_<%= product.id %>" class="block product" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
                                             <div class="product-image">
                                               <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
                                             </div>
-                                            <%= link_to truncate(product.name, :length => 50), product, :class => 'info', :itemprop => "name", :title => product.name %>
-                                            <span class="price selling" itemprop="price"><%= product.price_in(current_currency).display_price %></span>
                                           </div>
                                         <% end %>
                                       <% end %>
-                                      <% reset_cycle("classes") %>
                                     </ul>})
