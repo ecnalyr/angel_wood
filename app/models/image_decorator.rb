dc7d5c093fe3f2737971bb9803d8ccbf23387e45
Jason Rylance <ecnalyr@gmail.com>
2013-02-08 08:55:58 -0600

    Rearranged header.

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
     :small => '190x190>', # images on category view
-    :product => '570x570>', # full product image
+    :product => '575x575>', # full product image
     :large => '600x600>'  # light box image
   }
 end
--- app/overrides/shared/replace_header.rb
+++ app/overrides/shared/replace_header.rb
@@ -4,11 +4,11 @@ Deface::Override.new( :virtual_path => "spree/shared/_header",
                         :text => %q{<header id="header" class="row">
                                         <div class="navigation">
                                           <div class="custom_logo block"><h1><%= link_to AngelWood, root_path %></h1></div>
-                                          <div class="block blank_space"></div>
                                           <div class="block no_bottom"><h1><%= link_to "Gallery", root_path %></h1></div>
+                                          <div class="block"><h1><%= link_to "Contact", root_path %></h1></div>
                                           <div class="block blank_space">
                                             <li id="link-to-cart" data-hook><%= link_to_cart %></li>
                                           </div>
-                                          <div class="block"><h1><%= link_to "Contact", root_path %></h1></div>
+                                          <div class="block blank_space"></div>
                                         </div>
                                     </header>})
