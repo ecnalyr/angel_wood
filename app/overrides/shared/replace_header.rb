c625310588345e4e2ad596e8efae70822041a733
Jason Rylance <ecnalyr@gmail.com>
2013-02-09 12:03:40 -0600

    Continued editing colors

--- app/overrides/shared/replace_header.rb
+++ app/overrides/shared/replace_header.rb
@@ -4,13 +4,13 @@ Deface::Override.new( :virtual_path => "spree/shared/_header",
                         :text => %q{<header id="header" class="row">
                                         <div class="navigation">
                                           <%= link_to root_path do %>
-                                            <div class="custom_logo block"><h1>AngelWood</h1></div>
+                                            <div class="block" id="logo"><h1>AngelWood</h1></div>
                                           <% end %>
                                           <%= link_to root_path do %>
-                                            <div class="block no_bottom"><h1>Gallery</h1></div>
+                                            <div class="block no_bottom" id="gallery"><h1>Gallery</h1></div>
                                           <% end %>
                                           <%= link_to root_path do %>
-                                            <div class="block"><h1>Contact</h1></div>
+                                            <div class="block" id="contact"><h1>Contact</h1></div>
                                           <% end %>
                                           <div class="block blank_space">
                                             <li id="link-to-cart" data-hook><%= link_to_cart %></li>
