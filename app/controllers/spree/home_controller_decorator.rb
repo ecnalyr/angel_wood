dcf771af81ad313ecc4f35cdd42283152ee43b13
Jason Rylance <ecnalyr@gmail.com>
2013-02-11 09:14:27 -0600

    Added a simple 'for sale' flag to the product gallery

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,9 @@
+Spree::HomeController.class_eval do
+  def index
+      @searcher = Spree::Config.searcher_class.new(params)
+      @searcher.current_user = try_spree_current_user
+      @searcher.current_currency = current_currency
+      @products = @searcher.retrieve_products
+      respond_with(@products)
+    end
+end
