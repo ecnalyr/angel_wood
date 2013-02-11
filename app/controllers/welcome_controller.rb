519aa37f080b579d1b874fac522e4dfbad61f168
Jason Rylance <ecnalyr@gmail.com>
2013-02-11 07:02:30 -0600

    Added random product selection for welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@ class WelcomeController < Spree::StoreController
     layout 'application'
     respond_to :html
   def index
+    @searcher = Spree::Config.searcher_class.new(params)
+    @searcher.current_user = try_spree_current_user
+    @searcher.current_currency = current_currency
+    @products = @searcher.retrieve_products.limit(8).order('random()')
+    respond_with(@products)
   end
 end
