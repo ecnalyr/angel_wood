5067254a0ab186be3dab6924a3e0ea69c1871433
Jason Rylance <ecnalyr@gmail.com>
2013-02-13 10:13:03 -0600

    Added "Contact" link to footer.

--- app/overrides/shared/replace_footer.rb
+++ app/overrides/shared/replace_footer.rb
@@ -2,7 +2,12 @@ Deface::Override.new( :virtual_path => "spree/shared/_footer",
                         :replace => "footer#footer",
                         :name => "replace_footer",
                         :text => %q{<footer>
-                                        <div footer-left>
-                                            <p><%= t :Designed_by  %> <%= link_to 'Lost Tie', 'http://www.losttie.com/' %></p>
+                                        <div "id=footer-container">
+                                          <div id="footer-left">
+                                              <p><%= t :Designed_by  %> <%= link_to 'Lost Tie', 'http://www.losttie.com/' %></p>
+                                          </div>
+                                          <div id="footer-right">
+                                              <p><%= link_to 'Contact', main_app.contact_url %></p>
+                                          </div>
                                         </div>
                                     </footer>})
