d74b1770ff27ea85cdb565c4ec2f36eca72973ed
Jason Rylance <ecnalyr@gmail.com>
2013-02-15 07:40:26 -0600

    Ensured that at least 8 products load up on the welcome page (as long as there are at least 8 products in the db)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,9 @@ class WelcomeController < Spree::StoreController
     @searcher.current_user = try_spree_current_user
     @searcher.current_currency = current_currency
     @products = @searcher.retrieve_products.where(:worthy_of_welcome_page => true).limit(8).order('random()')
+    if @products.size < 8 # if there are fewer than 8 products marked as worthy_of_welcome_page
+      @products = @searcher.retrieve_products.limit(8).order('random()')
+    end
     respond_with(@products)
   end
 end
