be632a9c1b7699fb84d864c5d6bd30c69d730c81
Jason Rylance <ecnalyr@gmail.com>
2013-02-15 06:47:15 -0600

    Changed product images back to links even when 'for-sale' image covers them.  I did this for accessibility reasons.

--- app/overrides/shared/replace_products_ul.rb
+++ app/overrides/shared/replace_products_ul.rb
@@ -6,13 +6,11 @@ Deface::Override.new( :virtual_path => "spree/shared/_products",
                                         <% if product.on_display? %>
                                           <div id="product_<%= product.id %>" class="block product" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
                                             <div class="product-image">
+                                              <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
                                               <% if product.on_sale? %> 
-                                                <%= small_image(product, :itemprop => "image") %>
                                                 <div class="for-sale">
                                                   <%= link_to image_tag("for-sale.png"), product, :itemprop => 'url' %>
                                                 </div>
-                                              <% else %>
-                                                <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
                                               <% end %>
                                             </div>
                                           </div>
