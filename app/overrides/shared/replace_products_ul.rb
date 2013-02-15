5c6804a850ed90a5f02c5f0186f41ad3814f0628
Jason Rylance <ecnalyr@gmail.com>
2013-02-15 06:44:40 -0600

    Made for sale banners into links when appropriate.

--- app/overrides/shared/replace_products_ul.rb
+++ app/overrides/shared/replace_products_ul.rb
@@ -6,11 +6,13 @@ Deface::Override.new( :virtual_path => "spree/shared/_products",
                                         <% if product.on_display? %>
                                           <div id="product_<%= product.id %>" class="block product" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
                                             <div class="product-image">
-                                              <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
                                               <% if product.on_sale? %> 
+                                                <%= small_image(product, :itemprop => "image") %>
                                                 <div class="for-sale">
-                                                  <%= image_tag("for-sale.png") %>
+                                                  <%= link_to image_tag("for-sale.png"), product, :itemprop => 'url' %>
                                                 </div>
+                                              <% else %>
+                                                <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
                                               <% end %>
                                             </div>
                                           </div>
