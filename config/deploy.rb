baa54a62224b674622fd1c3c847017d28602717f
Jason Rylance <ecnalyr@gmail.com>
2013-02-16 13:55:37 -0600

    Corrected accidentally uncommented lines in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,35 +65,4 @@ default_environment["RAILS_ENV"] = 'production'
 #     sudo "bluepill load /data/#{application}/shared/config/#{application}.pill"
 #   end
 
-  desc "Start the application services"
-  task :start, :roles => :app do
-    sudo "bluepill #{application} start"
-  end
-
-  desc "Stop the application services"
-  task :stop, :roles => :app do
-    sudo "bluepill #{application} stop"
-  end
-
-  desc "Restart the application services"
-  task :restart, :roles => :app do
-    sudo "bluepill #{application} restart"
-  end
-end
-
-namespace :deploy do
-  desc "Symlink shared configs and folders on each release."
-  task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/Procfile #{release_path}/Procfile"
-    run "ln -nfs #{shared_path}/config/.foreman #{release_path}/.foreman"
-  end
-end
-
-before 'deploy:assets:precompile', 'deploy:symlink_shared'
-
-before 'deploy:start', 'foreman:export'
-after 'deploy:start', 'foreman:start'
-
-before 'deploy:restart', 'foreman:export'
-after 'deploy:restart', 'foreman:restart'
+ ?
