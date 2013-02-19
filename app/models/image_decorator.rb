78e0a2f3e8e6f6f6aff5fcb80d486adeb13eeb4d
Jason Rylance <ecnalyr@gmail.com>
2013-02-19 09:07:20 -0600

    Changed the default :small image definition to avoid 'edge clipping' on product images.

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
-    :small => '190x190#', # images on category view
+    :small => '190x190', # images on category view
     :product => '575x575>', # full product image
     :large => '600x600>'  # light box image
   }
